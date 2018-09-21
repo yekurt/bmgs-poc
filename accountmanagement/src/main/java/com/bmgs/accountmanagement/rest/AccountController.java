533be0fb0ee7428f39ea74a5068140b2d83717b0
Mahmut Sezenol <mahmutsezenol@gmail.com>
2018-09-21 15:06:06 +0300

    moved endpoints to related classes

---
+++ accountmanagement/src/main/java/com/bmgs/accountmanagement/rest/AccountController.java
@@ -0,0 +1,23 @@
+package com.bmgs.accountmanagement.rest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import reactor.core.publisher.Flux;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@RestController
+public class AccountController {
+
+    @Bean
+    RouterFunction<?> routes() {
+        return
+                route(GET("/accounts"), r -> ok().body(Flux.just("a", "b"), String.class))
+
+                ;
+
+    }
+}
